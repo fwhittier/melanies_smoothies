28456fa1d1bb5bb4c4f0aa41fec18178cf782c76
fwhittier <fwhittier@ghllc.com>
2025-08-22 14:40:26 -0500

    update df to use search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,9 @@ session = conn.session()
 
 # df = conn.query("SELECT * FROM mytable;", ttl="10m")
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
